259a62513aa08a21e821942b735b761326dd3548
mikaelelias <mikael.elias@gmail.com>
2023-04-20 21:56:17 -0300

    progress js

--- Calculator App/main.js
+++ Calculator App/main.js
@@ -8,6 +8,20 @@ function clean() {
 }
 
 function back() {
-    var resultado = resultado.document.getElementById('resultado').innerHTML = "";
+    var resultado = document.getElementById('resultado').innerHTML;
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() 
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+
+    else 
+    {
+        document.getElementById('resultado').innerHTML="";
+    }
 }
